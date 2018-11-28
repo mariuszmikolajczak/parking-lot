bca40f31dc5030c4e675e8b02563eb3302b9f081
Mariusz <mariusz@zdech.net>
2018-11-28 17:46:05 +0100

    Correct checkstyle

--- app/models/parking_spot.rb
+++ app/models/parking_spot.rb
@@ -3,5 +3,7 @@ class ParkingSpot < ApplicationRecord
   belongs_to :spot_size
   has_many :bookings
 
-  scope :all_by_level_and_size, -> { joins(:levels).joins(:spot_sizes).order('levels.number': :asc, 'spot_sizes.size': :asc) }
+  scope :all_by_level_and_size, lambda do
+    joins(:levels).joins(:spot_sizes).order('levels.number': :asc, 'spot_sizes.size': :asc)
+  end
 end
